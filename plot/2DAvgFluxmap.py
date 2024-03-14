127666a05801e748dc7bc7ca9081852e0f5545e1
LSP7654 <lillian.pullen@hotmail.com>
2024-03-14 08:51:18 -0400

    Avg Flux Plots make one graph

--- plot/2DAvgFluxmap.py
+++ plot/2DAvgFluxmap.py
@@ -27,27 +27,8 @@ planetname = "Earth"
 
 nfiles, nstars, starname, starradius, startemp, starcolor, fluxmax, fluxmin = infofile.read_infofile(prefix)
 
-# print(row.split(',')[0] row in infofile.read_infofile)
-# first_col = (int(row.split(',')[0]) for row in infofile.read_infofile(prefix))
-# fluxmin = fluxmax = next(first_col, None)
-# for i in first_col:
-#     if i < fluxmin:
-#         fluxmin = i
-#     elif i > fluxmax:
-#         fluxmax = i
-
-moviechoice = input("Make an animated gif at end? (y/n) ")
-deletechoice = 'n'
-if(moviechoice=='y'):
-    deletechoice = input("Delete .png files? (y/n) ")
-
-nzeros = int(np.log10(nfiles))+1
-
-# Loop over files
-print(range(nfiles))
-
 inputfile = prefix +'_'+planetname+'.avg'
-avgfluxfile = 'avgflux_'+prefix+'_'+planetname+'_'+num+'.png'
+avgfluxfile = 'avgflux_'+prefix+'_'+planetname+'.png'
 
 f = open(inputfile, 'r')
 
@@ -60,84 +41,24 @@ nlong = int(numbers[1])
         
 f.close()
 
+data = np.genfromtxt(inputfile, skip_header=1)
 
-for i in range(nfiles):
+latitude = data[:,latcol].reshape(nlat,nlong)*180.0/pi
+longitude= data[:,longcol].reshape(nlat,nlong)*180.0/pi
+avgflux = data[:,fluxcol].reshape(nlat,nlong)
+darkness = data[:,darkcol].reshape(nlat,nlong)
 
-    # Create filename - how many zeros needed?
-    num = str(i+1)
-    k=np.log10(i+1)
+fig1 = plt.figure(1)
+ax = fig1.add_subplot(111)
+ax.set_xlabel('Longitude (degrees)')
+ax.set_ylabel('Latitude (degrees)')
 
-       
-    ##For Avg Flux Plots 
-    #Written 1/25/24 by LSP7654
-    #inputfile = prefix +'_'+planetname+'_'+num+'.avg'
-    inputfile = prefix +'_'+planetname+'.avg'
-    avgfluxfile = 'avgflux_'+prefix+'_'+planetname+'_'+num+'.png'
+import matplotlib.colors as colors
 
-    # Read in header - time, position data etc
+print("max =", fluxmax)
+print("min =", fluxmin)
+pcm = ax.pcolor(longitude,latitude,avgflux,norm=colors.Normalize(vmin= fluxmin, vmax= fluxmax), cmap='Spectral')
+fig1.colorbar(pcm)
 
-    f = open(inputfile, 'r')
-
-    line = f.readline()
-
-    numbers=line.split() 
-
-    # time = 1
-    # print("warning: time is hardcoded T=1")
-    nlat = int(numbers[0])
-    nlong = int(numbers[1])
-        
-    f.close()
-    
-    #print('File', str(i+1),'Time', time, "yr")
-    
-    # Read in rest of file
-    
-    data = np.genfromtxt(inputfile, skip_header=1)
-        
-    # Reshape to fit 2D array
-    
-    latitude = data[:,latcol].reshape(nlat,nlong)*180.0/pi
-    longitude= data[:,longcol].reshape(nlat,nlong)*180.0/pi
-    avgflux = data[:,fluxcol].reshape(nlat,nlong)
-    darkness = data[:,darkcol].reshape(nlat,nlong)
-
-    # Plot 2D maps of this timestep    
-    
-    # Flux
-    
-    fig1 = plt.figure(1)
-    ax = fig1.add_subplot(111)
-    ax.set_xlabel('Longitude (degrees)')
-    ax.set_ylabel('Latitude (degrees)')
-    #vmax was set to fluxmax but I changed it
-    import matplotlib.colors as colors
-
-
-    print("max =", fluxmax)
-    print("min =", fluxmin)
-    pcm = ax.pcolor(longitude,latitude,avgflux,norm=colors.Normalize(vmin= fluxmin, vmax= fluxmax), cmap='Spectral')
-    fig1.colorbar(pcm)
-
-
-    plt.savefig(avgfluxfile, format= 'png')
-    plt.clf()
-
-# end of loop
-
-
-# Command for converting images into gifs - machine dependent
-
-convertcommand = '/opt/homebrew/Cellar/imagemagick/7.1.1-22/bin/convert '
-
-# Create movie if requested
-if(moviechoice=='y'):
-    print('Creating animated gif of avg flux pattern, filename avgfluxmovie.gif')
-    system(convertcommand +'-delay 10 avgflux_'+prefix+'*.png avgfluxmovie.gif')
-
-    if(deletechoice=='y'):
-        print('Deleting png files')
-        system('rm '+prefix+'_'+planetname+'*.png')                        
-        
-
-print('Complete')
+plt.savefig(avgfluxfile, format= 'png')
+plt.clf()
